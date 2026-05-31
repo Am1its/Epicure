f3ab3129381cae245eb21d2529d384bb954f9b38
Amit Oved <amit.oved@moveo.co.il>
2026-05-31 11:43:41 +0300

    feat: wire RestaurantsModule, ChefsModule, DishesModule into AppModule

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users.controller';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { ChefsModule } from './chefs/chefs.module';
+import { DishesModule } from './dishes/dishes.module';
 
 @Module({
-  imports: [],
+  imports: [RestaurantsModule, ChefsModule, DishesModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
