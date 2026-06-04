311a0b27ad901882d361f21284fee6c46ed5b1eb
Amit Oved <amit.oved@moveo.co.il>
2026-06-04 15:03:57 +0300

    chore: remove all spec and e2e test files

--- apps/frontend-e2e/src/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage has hero title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h1')).toContainText('Good food');
-});
